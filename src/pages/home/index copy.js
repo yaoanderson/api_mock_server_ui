82d5829960e4398b2aaa6ad70d59e2f891bef443
Anderson <anderson@AndersondeMacBook-Pro.local>
2025-06-02 22:53:03 +0800

    Update UI.

--- src/pages/home/index copy.js
+++ src/pages/home/index copy.js
@@ -21,25 +21,6 @@ import ControlPointIcon from '@material-ui/icons/ControlPoint';
 import SettingsIcon from '@material-ui/icons/Settings';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Backdrop from '@material-ui/core/Backdrop';
-import PropTypes from 'prop-types';
-import { alpha } from '@material-ui/core/styles';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { visuallyHidden } from '@material-ui/utils';
 
 import './home.css'
 import { logoutAction } from '../../common/action'
@@ -94,106 +75,6 @@ function a11yProps(index) {
     };
 }
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-  }
-  
-  function getComparator(order, orderBy) {
-    return order === 'desc'
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-  
-  const headCells = [
-    {
-      id: 'api',
-      numeric: false,
-      disablePadding: true,
-      label: 'API',
-    },
-    {
-      id: 'method',
-      numeric: false,
-      disablePadding: false,
-      label: 'METHOD',
-    },
-    {
-      id: 'mock',
-      numeric: true,
-      disablePadding: false,
-      label: 'MOCK',
-    },
-    {
-      id: 'config',
-      numeric: false,
-      disablePadding: false,
-      label: 'CONFIG',
-    }
-  ];
-  
-  function EnhancedTableHead(props) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-      props;
-    const createSortHandler = (property) => (event) => {
-      onRequestSort(event, property);
-    };
-  
-    return (
-      <TableHead>
-        <TableRow>
-          <TableCell padding="checkbox" style={{backgroundColor: "rgba(211, 211, 211, 0.42)", borderBottom: "1px solid rgb(204, 204, 204)"}}>
-            <Checkbox
-              color="primary"
-              indeterminate={numSelected > 0 && numSelected < rowCount}
-              checked={rowCount > 0 && numSelected === rowCount}
-              onChange={onSelectAllClick}
-              inputProps={{
-                'aria-label': 'select all desserts',
-              }}
-            />
-          </TableCell>
-          {headCells.map((headCell) => (
-            <TableCell
-              key={headCell.id}
-              align={headCell.id === 'api'? 'left': 'center'}
-              padding={headCell.disablePadding ? 'none' : 'normal'}
-              sortDirection={orderBy === headCell.id ? order : false}
-              style={{paddingRight: "0px", backgroundColor: "rgba(211, 211, 211, 0.42)", borderBottom: "1px solid rgb(204, 204, 204)", color: "rgba(0, 0, 0, 0.54)"}}
-            >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <Box component="span" sx={visuallyHidden}>
-                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                  </Box>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
-  }
-  
-  EnhancedTableHead.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-  };
-
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -204,60 +85,14 @@ class Home extends Component {
             progressIsDisplay: 'none',
             backdrop: false,
             apis: {},
-            api_methods: [],
+            api_ids: [],
             filterApis: [],
             tab: 0,
             api_all_enabled: false,
             token: props.token,
             refresh_token: props.refresh_token,
-            order: 'asc',
-            orderBy: 'api',
-            selected: [],
-            page: 0,
-            rowsPerPage: 5
         }
     }
-
-    handleRequestSort = (event, property) => {
-        const isAsc = this.state.orderBy === property && this.state.order === 'asc';
-        this.setState({
-          order: isAsc ? 'desc' : 'asc',
-          orderBy: property
-        });
-      };
-    
-      handleSelectAllClick = (event) => {
-        if (event.target.checked) {
-          this.setState({ selected: this.state.rows.map((row) => row.id) });
-        } else {
-          this.setState({ selected: [] });
-        }
-      };
-    
-      handleClick = (event, id) => {
-        const { selected } = this.state;
-        const selectedIndex = selected.indexOf(id);
-        let newSelected = [...selected];
-    
-        if (selectedIndex === -1) {
-          newSelected.push(id);
-        } else {
-          newSelected.splice(selectedIndex, 1);
-        }
-    
-        this.setState({ selected: newSelected });
-      };
-    
-      handleChangePage = (event, newPage) => {
-        this.setState({ page: newPage });
-      };
-    
-      handleChangeRowsPerPage = (event) => {
-        this.setState({
-          rowsPerPage: parseInt(event.target.value, 10),
-          page: 0
-        });
-      };
 
     componentDidMount() {
         if (this.state.debug || this.state.token) {
@@ -267,15 +102,6 @@ class Home extends Component {
     }
 
     render() {
-        const { order, orderBy, selected, page, rowsPerPage } = this.state;
-        var rows = this.state.filterApis || [];
-
-        const visibleRows = [...rows]
-            .sort(getComparator(order, orderBy))
-            .slice(page * rowsPerPage, (page + 1) * rowsPerPage);
-
-        const emptyRows = Math.max(0, rowsPerPage - visibleRows.length);
-
         return (
             <Fragment>
                 {this.state.redirect && <Redirect to="/login" /> }
@@ -294,8 +120,8 @@ class Home extends Component {
                         <Autocomplete
                             multiple
                             id="apis_selector"
-                            options={this.state.api_methods}
-                            getOptionLabel={(option) => `${option[0]} ${option[1]}`}
+                            options={this.state.api_ids}
+                            getOptionLabel={(option) => option}
                             defaultValue={[]}
                             renderInput={(params) => (
                                 <TextField
@@ -306,16 +132,13 @@ class Home extends Component {
                                     onKeyDown={(e) => {
                                         if (e.code === 'Enter' && e.target.value) {
                                             const apiList = e.target.value.split(',')
-                                            const newApiList = apiList.map(function (api_method) {
-                                                return api_method.trim()
+                                            const newApiList = apiList.map(function (api) {
+                                                return api.trim()
                                             })
                                             let filterApis = []
                                             while (newApiList.length > 0) {
-                                                const api_item = newApiList.pop();
-                                                const api_method = api_item.split(" ");
-                                                const new_api = api_method[0];
-                                                const new_method = api_method[1];
-                                                filterApis = filterApis.concat(this.state.api_methods.filter((api, method) => api === new_api && method === new_method))
+                                                const api_item = newApiList.pop()
+                                                filterApis = filterApis.concat(this.state.apis.filter(api => api === api_item))
                                             }
                                             this.setFilterApis(filterApis)
                                         }
@@ -326,7 +149,6 @@ class Home extends Component {
                             value={this.state.filterApis}
                             style={{ margin: 10, width: 980 }}
                         />
-
                         <List id="filterApis" >
                             <ListItem key={0} style={{ borderBottom: '1px solid #ccc', backgroundColor: '#d3d3d36b' }} >
                                 <ListItemText primary="API" style={{ overflow: 'auto', margin: 'auto 10px auto auto', color: 'rgba(0, 0, 0, 0.54)' }} />
@@ -364,92 +186,6 @@ class Home extends Component {
                                 )
                             }) }
                         </List>
-
-                        <TableContainer>
-                            <Table
-                                sx={{ minWidth: 750 }}
-                                aria-labelledby="tableTitle"
-                                size={'medium'}
-                            >
-                                <EnhancedTableHead
-                                numSelected={selected.length}
-                                order={order}
-                                orderBy={orderBy}
-                                onSelectAllClick={this.handleSelectAllClick}
-                                onRequestSort={this.handleRequestSort}
-                                rowCount={rows.length}
-                                />
-                                <TableBody>
-                                {visibleRows.map((row, index) => {
-                                    const isItemSelected = selected.includes(row);
-                                    const labelId = `enhanced-table-checkbox-${index}`;
-                                    const api = row[0];
-                                    const method = row[1];
-
-                                    return (
-                                    <TableRow
-                                        hover
-                                        onClick={(event) => this.handleClick(event, row)}
-                                        role="checkbox"
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        key={row}
-                                        selected={isItemSelected}
-                                        sx={{ cursor: 'pointer' }}
-                                    >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox
-                                                color="primary"
-                                                checked={isItemSelected}
-                                                inputProps={{
-                                                'aria-labelledby': labelId,
-                                                }}
-                                            />
-                                        </TableCell>
-                                        <TableCell
-                                        component="th"
-                                        id={labelId}
-                                        scope="row"
-                                        padding="none"
-                                        >
-                                            {api}
-                                        </TableCell>
-                                        <TableCell align="center">{method}</TableCell>
-                                        <TableCell align="center">
-                                            <Switch
-                                                color="primary"
-                                                edge="end"
-                                                onChange={(event) =>
-                                                    this.setStatus(index, event.target.checked)
-                                                }
-                                                checked={this.state.apis[api][method].enabled}
-                                            />
-                                        </TableCell>
-                                        <TableCell align="center">{this.state.apis[api][method].b}</TableCell>
-                                    </TableRow>
-                                    );
-                                })}
-                                {emptyRows > 0 && (
-                                    <TableRow
-                                    style={{
-                                        height: (53) * emptyRows,
-                                    }}
-                                    >
-                                    <TableCell colSpan={6} />
-                                    </TableRow>
-                                )}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        <TablePagination
-                        rowsPerPageOptions={[5, 10, 25]}
-                        component="div"
-                        count={rows.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={this.handleChangePage}
-                        onRowsPerPageChange={this.handleChangeRowsPerPage}
-                        />
                     </TabPanel>
                     <TabPanel value={this.state.tab} index={1} style={{width: 720, margin: '0 auto'}} >
                         Others Content
@@ -603,15 +339,15 @@ class Home extends Component {
                   throw new Error('Network response was not ok');
                 }
                 const jsonData = await response.json();
-                let api_methods = []
+                let api_ids = []
                 Object.entries(jsonData).forEach(([api, value]) => {
                     Object.keys(value).forEach(method => {
-                        api_methods.push([api, method])
+                        api_ids.push(`${api} ${method}`)
                     });
                 });
                 self.setState({
                     apis: jsonData,
-                    api_methods: api_methods
+                    api_ids: api_ids
                 });
             } catch (err) {
                 console.error(err.message);
@@ -620,7 +356,7 @@ class Home extends Component {
         else {
             self.setState({
                 apis: {},
-                api_methods: []
+                api_ids: []
             });
         }
     }
